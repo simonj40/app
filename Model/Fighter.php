50ae3db88164f42afff141b26d818d3506825fa2
kevllino <kevienid.eid@gmail.com>
2014-11-14 23:36:37 +0100

    doAttack
    Began the implementation of the do attack function

--- Model/Fighter.php
+++ Model/Fighter.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 App::uses('AppModel', 'Model');
 
@@ -26,7 +26,6 @@ class Fighter extends AppModel {
      * @todo empecher d'entrer sur une case occupée
      */
     public function doMove($fighterId, $direction){
-
         $this->read(null,$fighterId);
         
         if($direction=='north'){
@@ -39,12 +38,30 @@ class Fighter extends AppModel {
             $this->set('coordinate_x',$this->data['Fighter']['coordinate_x'] - 1);
         }
             $this->save();
+            
+    }
+    
 
+public function doAttack($id,$direction){
+    //successful attack 
+    //generate random number between 1 and 20
+    $al = rand(1,20);
+    // compute the success level
+    if($id == 1){
+    $limit = 10 + $this->data[$id]["Fighter"]["level"] - $this->data[$id-1]["Fighter"]["level"];
+    }
+    elseif($id==2){
+         $limit = 10 + $this->data[$id-1]["Fighter"]["level"] - $this->data[$id-2]["Fighter"]["level"];
+    }
+    //success condition
+    if($al >$limit){
+        //attackee looses a point of life 
+        $this-> set("current_health",$this->data[$id-1]["Fighter"]["current_health"]-1);
     }
     
-    public function doAttack($fighterId, $direction){
-        
+    $this->save();
     }
-    
+
+
 }
 
